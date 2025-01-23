6e8b26b102f30d9354e0117c894efdd0a0d4589a
surin <tjdtnfls1128@gmail.com>
2025-01-23 17:59:24 +0900

    About Break

---
+++ review/break.py
@@ -0,0 +1,10 @@
+for i in range(5, 10):
+    if i > 2:
+        if i > 3:
+            if i > 4:
+                print('어디?')
+                break
+            print('여기?!')
+        print('아님 여기?')
+
+print('여긴데~')
